1bb10fbcbcdf0c5aa8c5aa3d9d4f6f5fb58c40aa
fkhan698 <fkhan698@hotmail.com>
2020-07-03 12:32:32 -0500

    Intital resdesign commit

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-window.addEventListener("scroll", function() {
-    const distance = window.scrollY;
-    document.querySelector("banner").style.transform = `translateY(${distance *
-      1}px)`;
-    document.querySelector(
-      ".container"
-    ).style.transform = `translateY(${distance * 0.3}px)`;
-    setTimeout(() => {
-      document.querySelector("about ").classList.add("animate-me");
-    }, 400);
-  });
-  
+VANTA.WAVES({
+  el: "#waves",
+  mouseControls: true,
+  touchControls: true,
+  minHeight: 600.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xa293c,
+  shininess: 87.00,
+  waveSpeed: 0.75
+})
+
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    target.classList.add('active')
+  })
+})
